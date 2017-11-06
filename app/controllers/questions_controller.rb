4e672277222bdec7f32434667281cf6e67b7197f
Eduardo Regalo <ehregalo@gmail.com>
2017-11-06 13:55:44 -0200

    creating htmls with Rails

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @query = params[:query]
+    @answer = coach_answer(@query)
+  end
+
+  def coach_answer(your_message)
+    if your_message == "I am going to work right now!"
+      return "Ok."
+    elsif your_message.include? "?"
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+end
